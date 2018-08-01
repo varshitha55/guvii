2408e6c4a82d33c27904eca8d334017ab438b1ba
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-01 14:14:28 +0530

    Update and rename README.md to Character.java

---
+++ Character.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Character
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int count=0;
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)==' ' && str.charAt(i+1)!=' ')
+	            	{
+			
+			}
+			else
+			count++;
+		}
+		System.out.println(count);
+	}
+}
