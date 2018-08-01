33347869cdd1b30964a7b827140a3db78380b24c
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-01 14:35:10 +0530

    Update and rename Character.java to lines.java

--- lines.java
+++ lines.java
@@ -3,23 +3,29 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Character
+class lines
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
-		int count=0;
-		for(int i=0;i<str.length();i++)
+		int count=1;
+		for(int i=1;i<str.length();i++)
 		{
-			if(str.charAt(i)==' ' && str.charAt(i+1)!=' ')
+			if(str.charAt(i)=='.' && str.charAt(i+1)!=' ')
 	            	{
-			
+				count++;
 			}
-			else
-			count++;
-		}
+			else if(str.charAt(i)==' ' && str.charAt(i+1)==' ')
+			{
+				count++;
+		            }
+		            else if(str.charAt(i)=='.')
+		            {
+		            	count++;
+		            }
+		}           
 		System.out.println(count);
 	}
 }
