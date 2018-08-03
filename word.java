8addea3b9965670002321a7348412960a637588e
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-03 15:26:20 +0530

    Update and rename fibnoci.java to word.java

---
+++ word.java
@@ -0,0 +1,42 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class word
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			Scanner in=new Scanner(System.in);
+		int input=in.nextInt();
+		switch(input)
+		{
+		
+			case 1:System.out.println("one");
+				break;
+			case 2:System.out.println("two");
+				break;
+			case 3:System.out.println("three");
+				break;
+			case 4:System.out.println("four");
+				break;
+			case 5:System.out.println("five");
+				break;
+			case 6:System.out.println("six");
+				break;
+			case 7:System.out.println("seven");
+				break;
+			case 8:System.out.println("eight");
+				break;
+			case 9:System.out.println("nine");
+				break;
+			case 10:System.out.println("ten");
+				break;
+		}	
+				
+	}		
+}			
+		
