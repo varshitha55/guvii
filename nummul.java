946150d27d4176b08f4a2894eae33d608dd18bba
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-03 14:45:10 +0530

    Update and rename multiple.java to nummul.java

--- nummul.java
+++ nummul.java
@@ -5,15 +5,16 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class multiple
+class nummul
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
-			int n=sc.nextInt();
-		n=(n/10)*10;
-		n+=10;
-		System.out.println(n);
+		int n=sc.nextInt();
+		if(n%7==0)
+			System.out.println("yes");
+		else 
+			System.out.println("no");
 	}
 }
