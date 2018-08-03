f7d8cd2dace613fee154dbb8bf4759d3509aafb5
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-03 14:28:23 +0530

    Update and rename odd.java to semi.java

--- semi.java
+++ semi.java
@@ -4,18 +4,13 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class odd
+class semi
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		
 		Scanner sc=new Scanner(System.in);
-				int n=sc.nextInt();
-		int m=sc.nextInt();
-		int sum=n+m;
-		if(sum%2==0)
-			System.out.println("even");
-		else
-			System.out.println("odd");
+			String str=sc.nextLine();
+		System.out.println(str);
 	}
-}
+}		
