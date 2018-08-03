2966f69f9acc48c6ccc4bdfd0562094d84fda671
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-03 15:44:53 +0530

    Update and rename next.java to sum2.java

--- sum2.java
+++ sum2.java
@@ -5,14 +5,16 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class next
+class sum2
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
+		
 		int n=sc.nextInt();
-		n++;
-		System.out.println(n);
+		int n1=sc.nextInt();
+		int sum=n1+n;
+		System.out.println(sum);
 	}
 }	
