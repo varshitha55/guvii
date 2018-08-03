12604057e804143fafc6ab32e1d071ca8aad5b79
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-03 15:36:35 +0530

    Update and rename word.java to count.java

---
+++ count.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class count
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int rem=0,cnt=0;
+		while(n>0)
+		{
+			rem=n%10;
+			cnt++;
+			n=n/10;
+		}
+		System.out.println(cnt);
+	}
+}	
