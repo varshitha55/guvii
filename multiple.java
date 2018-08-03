8eb441c67e67f8c2921fa51a581cc3053ba51e13
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-03 14:41:07 +0530

    Update and rename prime.java to multiple.java

--- multiple.java
+++ multiple.java
@@ -5,22 +5,15 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class prime
+class multiple
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int count=0;
-		for(int i=1;i<=n;i++)
-		{
-				if(n%1==0)
-				count++;
-		}
-		if(count==2)
-			System.out.println("yes");
-		else
-			System.out.println("no");
+			int n=sc.nextInt();
+		n=(n/10)*10;
+		n+=10;
+		System.out.println(n);
 	}
 }
