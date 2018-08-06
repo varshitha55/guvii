8981cac9f676fee761b6b6a92ddb1a0e4db6822c
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-06 13:38:32 +0530

    Update and rename mod.java to diff.java

--- diff.java
+++ diff.java
@@ -3,25 +3,16 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class mod
+class diff
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		
 		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
-		char c=sc.next().charAt(0);
-		int b=sc.nextInt();
-		switch(c)
-		{
-				case'/':int res=a/b;
-				System.out.println(res);
-				break;
-					case'%':res=a%b;
-				System.out.println(res);
-				break;
-				
-		}
+			int num1=sc.nextInt();
+		int num2=sc.nextInt();
+		int num=Math.abs(num1-num2);
+		System.out.println(num);
 	}
 }
