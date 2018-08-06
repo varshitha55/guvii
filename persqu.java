0d9a5923d75fe76309e31fd3da4850681dc0b755
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-06 13:47:05 +0530

    Update and rename diff.java to persqu.java

--- persqu.java
+++ persqu.java
@@ -3,16 +3,20 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class diff
+class persqu
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		
 		Scanner sc=new Scanner(System.in);
-			int num1=sc.nextInt();
-		int num2=sc.nextInt();
-		int num=Math.abs(num1-num2);
-		System.out.println(num);
+				double num1=sc.nextDouble();
+		double num2=sc.nextDouble();
+		double pro=num1*num2;
+		double sr=Math.sqrt(pro);
+		if(sr-Math.floor(sr)==0)
+			System.out.println("yes");
+		else 
+			System.out.println("no");
 	}
 }
